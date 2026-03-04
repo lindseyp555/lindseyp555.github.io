ff1dd1bdd9a1a5452f91ebfa6c2f42deedebf0cf
Lindsey Peterson <lindseyp555@gmail.com>
2026-03-03 23:11:56 -0700

    setting name back

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
             Projects
           </Link>
           <a
-            href="/Resume.docx.pdf"
+            href="/Resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
           >
